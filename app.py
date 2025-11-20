e106e1e74a6159f89da6f55a32af93a6660af57f
apurva123-hub <apurvadoijad28@gmail.com>
2025-11-20 16:43:05 +0530

    Rag

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+import streamlit as st
+from langchain_cohere import CohereEmbeddings
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+
+
